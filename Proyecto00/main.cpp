41ee24b1a1ca03fc5917b4f6110e6f1cbe11faf6
Esteban Navas <198956144+blqnce@users.noreply.github.com>
2025-08-04 23:20:51 +0000

    aa

--- Proyecto00/main.cpp
+++ Proyecto00/main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 //Funcion para la implementacion de la consola
-vector<string> userIn();
+vector<string> entrada();
 
 int main(int argc, char *argv[]) { 
-  //Se realiza un bucle "permanente" en el sistema para que la unica forma de salir del programa sea con salir.
+  //Se realiza un bucle "permanente" en el sistema para que la unica forma de salir del programa sea con la indicacion del usuario.
   while (true) {
-    vector<string> comando = userIn();
+    vector<string> comando = entrada();
     if (comando[0] == "salir") {
             exit(0);
     }
@@ -24,19 +24,19 @@ int main(int argc, char *argv[]) {
             cout << "\n========== LISTA DE COMANDOS ==========" << endl;
             cout << "- cargar <nombre_archivo>" << endl;
             cout << "- listar_secuencias" << endl;
-            cout << "- salir\" << endl;
+            cout << "- salir" << endl;
             cout << "=======================================" << endl;
             cout << "Use 'ayuda <comando>' para obtener información detallada." << endl;
     }
     else if (comando[0] == "ayuda" && comando.size() > 1) {
             if (comando[1] == "cargar")
-                cout << "\n(cargar <nombre_archivo>)\nCarga un archivo en el sistema." << endl;
+                cout << "(cargar <nombre_archivo>)\nCarga un archivo en el sistema." << endl;
             else if (comando[1] == "listar_secuencias")
-                cout << "\n(listar_secuencias)\nImprime la cantidad de secuencias en memoria y el contenido de estas." << endl;
+                cout << "(listar_secuencias)\nImprime la cantidad de secuencias en memoria y el contenido de estas." << endl;
             else if (comando[1] == "salir")
-                cout << "\n(salir)\nTermina la ejecucion del programa." << endl;
+                cout << "(salir)\nTermina la ejecucion del programa." << endl;
             else
-                cout << "\nError: Comando inexistente.\n";
+                cout << "Error: Comando inexistente.\n";
     }
     else {
             cout << "\nError: Comando inválido.\n";
@@ -46,18 +46,21 @@ int main(int argc, char *argv[]) {
   
 }
 
-//Las entradas del usuario se trabajan mediante vectores para identificar los inputs y los parametros que se pasan al programa.
+//Las entradas del usuario se trabajan mediante vectores para identificar las entradas y los parametros que se pasan al programa.
 
-vector<string> userIn() {
+vector<string> entrada() {
     string in;
     do{
       cout << "$ ";
       getline(cin, in);} 
     while (in.empty());
-  
+    // Para manejar la cantidad de argumentos utilizamos stringstream, donde podemos separar las entradas a la consola y almacenarlas en el vector.
     stringstream ss(in);
+    // La estructura de el vector (y la entrada por consola) es: <nombre_comando> <parametro1> <parametro2> ... <parametron>
     vector<string> comando;
     while (ss >> in) {
+        // El tamaño del vector comando indica el numero de parametros, por ejemplo si un comando necesita un parametro nombre_archivo el vector seria de tamaño 2, donde la primera posicion
+        // es el comando y la segunda posicion el parametro nombre_archivo
         comando.push_back(in);
     }
     return comando;
